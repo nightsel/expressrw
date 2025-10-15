9522b5c20fbd7c16df97c350e7c46217fc267ab4
Max <you@example.com>
2025-10-15 11:26:04 +0300

    aeneastest

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, send_file
 import requests
 from tempfile import NamedTemporaryFile
 from tempfile import gettempdir
